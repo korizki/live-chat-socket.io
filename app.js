4c4702c2064ffbca0b53a2e892a94bc7fddfe24a
Rizki Ramadhan <66109920+korizki@users.noreply.github.com>
2023-03-15 14:41:34 +0800

    Update code

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const io = new Server(server)
 app.use(express.static("public"))
 // menggunakan socket io
 io.on("connection", (socket) => {
-    socket.on("chat", arg => {
+    socket.on("chat", message => {
+        console.log(message)
         io.emit("chatToClient", {id: socket.id, message})
     })
     socket.on("disconnect", () => console.log("Disconnected from server."))
