98549a5d43d31639808c717a04a00dd109655658
Rizki Ramadhan <66109920+korizki@users.noreply.github.com>
2023-03-15 13:44:28 +0800

    Inisialisasi file app.js (Server/Backend) dengan Socket IO

---
+++ app.js
@@ -0,0 +1,19 @@
+// menggunakan dependencies/module
+const express = require('express')
+const http = require('http')
+const { Server } = require('socket.io')
+// create server
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
+// menggunakan midleware http - set folder public as root 
+app.use(express.static("public"))
+// menggunakan socket io
+io.on("connection", (socket) => {
+    socket.on("chat", message => {
+        io.emit("chatToClient", {id: socket.id, message})
+    })
+    console.log("Connected on port 3000")
+})
+// inisialisasi port server
+server.listen(3000)
